1050e20193bbd423ff130e199881b66885d94608
khalid <safarovkhalid0@gmail.com>
2026-08-27 08:09:41 +0500

    added html & js

---
+++ Khallid/homeWork.js
@@ -0,0 +1,122 @@
+console.log(`\nTask 21`);
+let age = 17
+if (age>=18) console.log("Метавонад овоз диҳад");
+else console.log("Наметавонад овоз диҳад");
+
+
+console.log(`\nTask 22`);
+let number = 15
+if (number%5==0) console.log("Ба 5 тақсим мешавад");
+else console.log("Ба 5 тақсим намешавад");
+
+
+console.log(`\nTask 23`);
+let temperature = -5
+if (temperature<0) console.log("Мороз");
+else console.log("Тепло");
+
+
+console.log(`\nTask 24`);
+let password = "1234"
+if (password=="1234") console.log("Доступ разрешён");
+else console.log("Доступ запрещён");
+
+
+console.log(`\nTask 25`);
+let score = 75
+if (score>=70) console.log("Хорошо");
+else console.log("Плохо");
+
+
+console.log(`\nTask 26`);
+let isWeekend = false
+if (isWeekend===false) console.log("Работаем!");
+else console.log("Отдыхаем!");
+
+
+console.log(`\nTask 27`);
+let year = 2024
+if (year % 4 == 0 * year % 100 != 0) console.log("Високосный");
+else console.log("Не високосный");
+
+
+console.log(`\nTask 28`);
+let height = 150
+if (height>=150) console.log("Можно кататься");
+else console.log("Нельзя кататься");
+
+
+console.log(`\nTask 29`);
+let age2 = 6
+if (age2>=7) console.log("В школу");
+else console.log("Ещё рано");
+
+
+console.log(`\nTask 30`);
+let money = 300
+if (money>=250) console.log("Хватит");
+else console.log("Не хватит");
+
+
+console.log(`\nTask 31`);
+let number2 = 18
+if (number2>10 & number2<20) console.log("В диапазоне");
+else console.log("Вне диапазона");
+
+
+console.log(`\nTask 32`);
+let isMember = true
+if (isMember===true) console.log("Тахфифи 10%");
+else console.log("Тахфиф нест");
+
+
+
+console.log(`\nTask 33`);
+let time = 22
+if (time == 22) console.log("Поздно");
+else console.log("Ещё рано");
+
+
+
+console.log(`\nTask 34`);
+let grade = 95
+if (grade>=90) console.log("Отлично!");
+else console.log("Хорошо");
+
+
+console.log(`\nTask 35`);
+let distance = 5   
+if (distance<10) console.log("Близко");
+else console.log("Далеко");
+
+
+console.log(`\nTask 36`);
+let hasLicense = false
+if (hasLicense==false) console.log("Нельзя водить");
+else console.log("Можно водить");
+
+
+
+console.log(`\nTask 37`);
+let age4= 13
+if (age4>=13 & age4<=19) console.log("Подросток");
+else console.log("Не подросток");
+
+
+console.log(`\nTask 38`);
+let score22 = 55
+if (score22>=50) console.log("Сдал");
+else console.log("Не сдал");
+
+
+console.log(`\nTask 39`);
+let isDay = true
+if (isDay==true) console.log("Доброе утро!");
+else console.log("Добрый вечер");
+
+
+console.log(`\nTask 40`);
+let numbera = 7
+if (numbera/1==numbera & numbera/numbera==1) console.log("Содда");
+else console.log("Мураккаб");
+
