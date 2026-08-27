09753e332ca856fe76bf869b9a0d58fd6012ced3
khalid <safarovkhalid0@gmail.com>
2026-08-27 10:29:49 +0500

    extraWork js

---
+++ Khallid/extraWork.js
@@ -0,0 +1,90 @@
+console.log(`\nTask 41`);
+let a = 10
+let b = 20
+if (a>b)console.log("a больше");
+else if (a==b)console.log("Равны");
+else console.log("b больше");
+
+
+console.log(`\nTask 42`);
+let age = 20
+if (age>=16)console.log("Может голосовать: true");
+if (age<=20) console.log("Может водить: true");
+
+console.log(`\nTask 43`);
+let number = 9
+if (number>=5 & number<=15)console.log("В диапазоне");
+else console.log("Берун аз доира");
+
+
+console.log(`\nTask 44`);
+let number1 = 12
+if (number%2==0 & number%3==0)console.log("Ба 2 ва 3 тақсим мешавад");
+else console.log("Тақсим намешавад");
+
+
+
+console.log(`\nTask 45`);
+let day = "понедельник"
+if (day=="понедельник")console.log("Будний");
+else console.log("Выходной");
+
+
+
+console.log(`\nTask 46`);
+let temperature = 35
+if (temperature>30)console.log("Гарм");
+else if (temperature>20)console.log("Нарм");
+else console.log("Сард");
+
+
+console.log(`\nTask 47`);
+let score = 85
+if (score>=90)console.log("Аъло");
+else if (score>=75)console.log("Хуб");
+else if (score>=50) console.log("Қаноатбахш");
+
+
+
+console.log(`\nTask 48`);
+let number2 = -5
+if ( number2>0)console.log("positive");
+else if (number2<0)console.log("negative");
+else if (number2==0) console.log("zero");
+
+
+console.log(`\nTask 49`);
+let age5 = 25
+if (age5>0 & age5<=12)console.log("Ребёнок");
+else if (age5>=13 & age5<=19)console.log("Подросток");
+if (age5>=20 & age5<=59)console.log("Взрослый");
+if (age5>=60)console.log("Пожилой");
+
+
+console.log(`\nTask 50`);
+let height = 170
+if (height>180)console.log("Очень высокий");
+else if (height>160)console.log("Высокий");
+else if (height>150)console.log("Средний");
+else console.log("Низкий");
+
+
+
+console.log(`\nTask 51`);
+let year = 2024
+if (year % 4 == 0 * year % 100 != 0) console.log("Високосный");
+else console.log("Не високосный");
+
+
+console.log(`\nTask 52`);
+let isWeekend = true
+let isHoliday = false
+if (isWeekend==true || isHoliday==true) console.log("Отдыхает");
+else console.log("Работает");
+
+
+console.log(`\nTask 53`);
+let number3 = 7
+if (number3%2!==0 & number3>=5) console.log("Ҳа");
+else console.log("Не");
+
